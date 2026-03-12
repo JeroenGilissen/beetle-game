e22e198a9e4a54d7d69afd8e6595a3c694603d67
arnobarzan <arno.barzan@pxl.be>
2026-03-12 11:43:23 +0100

    Add tips in Beetle.cs

--- BeetleGame/Beetle.cs
+++ BeetleGame/Beetle.cs
@@ -8,7 +8,14 @@ namespace BeetleGame
 {
     public class Beetle
     {
-       
-
+        // Tip: to fix the first 3 tests of Beetle
+        // 1. Add a constructor with 4 parameters (Canvas, int , int , int)
+        // 2. Add PROPERTIES:
+        //      double Speed
+        //      int Size
+        //      int Y
+        //      bool Right
+        //      bool Up 
+        //      bool IsVisible
     }
 }
